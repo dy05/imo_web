2b9a07730f528f0fa193a8ed6d94d87c24564f6c
fwdilando111 <52221649+fwdilando111@users.noreply.github.com>
2019-12-04 16:55:05 +0100

    modification du footer et app dans le nouveaux projet

--- Views/contact.php
+++ Views/contact.php
@@ -5,9 +5,17 @@ $template = 'layouts/app.php';
 <section>
     <h2>Contactez Nous</h2>
     <form action="post">
-        <input type="text" placeholder="Name">
-
-        <textarea name="message" id="" cols="30" rows="10" placeholder="Laissez un message"></textarea>
-        <button>Validate</button>
+        <div class="col col-lg-5 form-group">
+            <label for="pseudo">Pseudo</label>
+            <input type="text" placeholder="pseudo" id="pseudo" class="form-control">
+        </div>
+        <div class="col col-lg-5 form-group">
+            <label for="email">Email</label>
+            <input type="email" placeholder="email" id="email" class="form-control">
+        </div>
+        <div class="col col-lg-6 form-group">
+            <textarea name="message" id="" cols="30" rows="10" placeholder="Laissez un message" class="form-control"></textarea>
+        </div>
+        <button class="btn btn-success col-lg-2">Validate</button>
     </form>
 </section>
--- Views/layouts/app.php
+++ Views/layouts/app.php
@@ -37,128 +37,88 @@
 </main>
 
     <!-- Footer -->
-    <footer class="page-footer font-small mdb-color lighten-3 pt-4">
-        <div class="container text-center text-md-left">
-            <div c<!-- Grid column -->
-            <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
-
-                <!-- Content -->
-                <h5 class="font-weight-bold text-uppercase mb-4">Footer Content</h5>
+     <footer class="page-footer font-small mdb-color lighten-3 pt-4">
+    <div class="container text-center text-md-left">
+        <div class="row">
+            <div class="col col-lg-3">
+                <h5 class="font-weight-bold text-uppercase mb-4">Platform Immobilier </h5>
                 <p>Here you can use rows and columns to organize your footer content.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
-                    esse
-                    quasi, veritatis totam voluptas nostrum.</p>
-
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate .</p>
             </div>
-            <!-- Grid column -->
-
-            <hr class="clearfix w-100 d-md-none">
-
-            <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
-
-                <!-- Links -->
+            <div class="col col-lg-3">
                 <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
-
                 <ul class="list-unstyled">
                     <li>
                         <p>
-                            <a href="#!">PROJECTS</a>
+                            <a href="#!">Achat</a>
                         </p>
                     </li>
                     <li>
                         <p>
-                            <a href="#!">ABOUT US</a>
+                            <a href="#!">Vente</a>
                         </p>
                     </li>
                     <li>
                         <p>
-                            <a href="#!">BLOG</a>
+                            <a href="#!">A propos</a>
                         </p>
                     </li>
                     <li>
                         <p>
-                            <a href="#!">AWARDS</a>
+                            <a href="#!">FAQ</a>
                         </p>
                     </li>
                 </ul>
 
             </div>
-            <!-- Grid column -->
-
-            <hr class="clearfix w-100 d-md-none">
-
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
-
-                <!-- Contact details -->
+            <div class="col col-lg-3">
                 <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
-
                 <ul class="list-unstyled">
                     <li>
                         <p>
-                            <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                            <i class="fas fa-home mr-3"></i>Douala, NY 10012, CM</p>
                     </li>
                     <li>
                         <p>
-                            <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+                            <i class="fas fa-envelope mr-3"></i>platformimmobilier@gmail.com</p>
                     </li>
                     <li>
                         <p>
-                            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                            <i class="fas fa-phone mr-3"></i> + 22 234 567 88</p>
                     </li>
                     <li>
                         <p>
-                            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                            <i class="fas fa-print mr-3"></i> + 22 234 567 89</p>
                     </li>
                 </ul>
 
             </div>
-            <!-- Grid column -->
-
-            <hr class="clearfix w-100 d-md-none">
-
-            <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
-
-                <!-- Social buttons -->
+            <div class="col-lg-3">
                 <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
-
-                <!-- Facebook -->
                 <a type="button" class="btn-floating btn-fb">
                     <i class="fab fa-facebook-f"></i>
                 </a>
-                <!-- Twitter -->
                 <a type="button" class="btn-floating btn-tw">
                     <i class="fab fa-twitter"></i>
                 </a>
-                <!-- Google +-->
                 <a type="button" class="btn-floating btn-gplus">
                     <i class="fab fa-google-plus-g"></i>
                 </a>
-                <!-- Dribbble -->
                 <a type="button" class="btn-floating btn-dribbble">
                     <i class="fab fa-dribbble"></i>
                 </a>
-
             </div>
-            <!-- Grid column -->
-
         </div>
-        <!-- Grid row -->
+    </div>
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 bg-#42127b">
+        &copy; FindIt <?= date('Y'); ?> -  Tous droits reserves.
+        <a href="#">IUC-TI-PAM2-1920</a>
+    </div>
+    <!-- Copyright -->
 
-        </div>
-        <!-- Footer Links -->
-
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">
-            &copy; FindIt <?= date('Y'); ?> -  Tous droits reserves.
-            <a href="#">IUC-TI-PAM2-1920</a>
-        </div>
-        <!-- Copyright -->
-
-    </footer>
-    <!-- Footer -->
+</footer>
+     <!-- Footer -->
 
 
 <!--lien de bibiotheque js-->
